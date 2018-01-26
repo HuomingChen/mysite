751a3395c4cf9d588c26a2cf191966b40e3229b1
HuomingChen <gamefile1@163.com>
2018-01-26 23:16:16 +0800

    。。。

--- login/send_mail.py
+++ login/send_mail.py
@@ -6,7 +6,7 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 if __name__ == '__main__':
 
     send_mail(
-        '来自',
+        '来自1',
         '欢迎访问',
         'gamefile6@163.com',
         ['gamefile1@163.com'],
