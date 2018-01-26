20ac2cc9287d4a6ded8cf88673d4a4f39fb42374
HuomingChen <gamefile1@163.com>
2018-01-26 23:07:36 +0800

    Initial commit

---
+++ login/send_mail.py
@@ -0,0 +1,13 @@
+import os
+from django.core.mail import send_mail
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+if __name__ == '__main__':
+
+    send_mail(
+        '来自',
+        '欢迎访问',
+        'gamefile6@163.com',
+        ['gamefile1@163.com'],
+    )
---
+++ mysite/__init__.py
@@ -0,0 +1,11 @@
+
+#python manage.py makemigrations polls  根据model定义， 创建模型
+
+#python manage.py sqlmigrate polls 0001   可以查看执行的语句命令
+
+#python manage.py check  可以检查是否有执行错误
+
+#python manage.py migrate   执行创建表
+
+#pip freeze  > ./requirements.txt 创建环境依赖文件
+# pip install -r requirements.txt 安装环境
